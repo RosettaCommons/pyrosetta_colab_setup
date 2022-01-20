5526b5afa1db8a8bb24ec9f8aa1a8af7a37d7554
Sergey Lyskov <3302736+lyskov@users.noreply.github.com>
2022-01-20 13:40:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyrosettacolabsetup',
-      version='0.7',
+      version='0.8',
       description='Mounts Google Drive for PyRosetta use in Google Colaboratory',
       url='https://github.com/kathyle9/pyrosettacolabsetup',
       author='kathyle9',
